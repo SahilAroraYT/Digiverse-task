642d88009397ffb503e315f0f324a675ea440f61
Sahil Arora <sahilarorapersonal2003@gmail.com>
2025-07-24 22:28:32 +0530

    server the req.body as json

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const path = require('path');
 
 dotenv.config()
 
+app.use(express.json());
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
